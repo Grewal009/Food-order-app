1800099eb55f14e9cf0ce968748db1f33680aee6
aneel.grewal <aneel.grewal@hotmail.com>
2023-03-01 12:03:28 +0100

    fetching data from the API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,32 @@
 
 import RestaurantCard from "./RestaurantCard";
 import { RESTRO_DATA } from "../utils/Constants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const restro_List = RESTRO_DATA;
+  //const restro_List = RESTRO_DATA;
 
   const [searchText, setSearchText] = useState("");
-  const [restaurants, setRestaurants] = useState(restro_List);
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(()=>{
+    getRestaurants();
+  },[]);
+
+  async function getRestaurants(){
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json();
+    //console.log(json.data.cards[2].data.data.cards);
+    setRestaurants(json.data.cards[2].data.data.cards);
+
+  }
 
   function filterData(searchText, restaurants){
    return restaurants.filter((rest)=> rest.data.name.toLowerCase().includes(searchText.toLowerCase()));
     }
   
-  return (
+  return(
     <div>
         <div className="bg-yellow-200 p-5 flex justify-center items-center ">
           <input className="border-2 border-gray-300 rounded-md px-2 active:border-blue-500
@@ -21,7 +34,7 @@ const Body = () => {
           onChange={(e)=>setSearchText(e.target.value)} />
           <button className="mx-2 border-2 px-3 py-1 bg-indigo-500 hover:bg-indigo-800 rounded-md font-bold text-white" 
           onClick={()=>{
-            const data = filterData(searchText, restro_List);
+            const data = filterData(searchText, restaurants);
             console.log(data);
             setRestaurants(data);
             }}>Search</button>
@@ -29,8 +42,10 @@ const Body = () => {
         </div>
 
       <div className="bg-yellow-200 min-h-screen px-5 flex flex-wrap justify-center">
-
-        { restaurants.map(
+      
+        { (restaurants.length === 0)? <Shimmer />
+      :
+         restaurants.map(
           (rest)=><RestaurantCard restaurant={rest} key={rest.data.id}/>
         )}
       
