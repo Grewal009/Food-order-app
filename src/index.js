e3d7f0a12104b28ba5d807d312c9f5d2417bdd0a
aneel.grewal <aneel.grewal@hotmail.com>
2023-03-03 00:12:49 +0100

    router configuration created by createBrowserRouter  and passed to RouterProvider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 import React from 'react'
 import ReactDOM  from 'react-dom/client';
-import App from './components/App';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
+import About from './components/About';
+import Contact from './components/Contact';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
+const AppLayout = () => {
+  return (
+      <div>
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    )
+}
 
-
+const appRouter = createBrowserRouter([
+    {
+        path : "/",
+        element : <AppLayout />,
+        
+    },
+    {
+        path : "/about",
+        element : <About />,
+    },
+    {
+        path : "/contact",
+        element : <Contact />,
+    }
+]);
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
